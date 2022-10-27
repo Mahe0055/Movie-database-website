409e8a2152c2d9964cd55d6aa98e62c8febc6429
Mahe0055 <mahe00555@stud.kea.dk>
2022-10-27 17:20:20 +0200

    exercise 4

--- main.js
+++ main.js
@@ -4,18 +4,42 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
     .then(movies => {
         console.log(movies);
 
-//Exercise 2 - level 1 Render string "6527 movies fetched" to HTML.
+        //Exercise 2 - level 1 Render string "6527 movies fetched" to HTML.
         const h2 = document.querySelector("h2")
         h2.innerHTML = movies.length+" movies fetched";
         document.body.appendChild(h2);
 
+
 //Exercise 3 - level 1 Render 1. movie in the movies array.
         const h3 = document.querySelector("h3")
         h3.innerHTML = JSON.stringify(movies[0]);
+        h3.innerText = "The movie '71' got released in 2014. The rating is 7.2 by 41702 votes in total. The movie last 5940 seconds.";
         document.body.appendChild(h3);
 
-//Exercise 4 - level 2
-        const ul = document.querySelector("ul")
-        ul.innerHTML = JSON.stringify(movies);
-        document.body.appendChild(ul);
-});
+//Exercise 4 - level 2 Render the movies in a list.
+        const listOfMovies = document.querySelector("#list");
+        for (let i = 0; i < movies.length; i++){
+                const myElement = document.createElement("li");
+                let content = movies[i]['title'] + ', ' + movies[i]['year'] + ', ' + movies[i]['rating'] + ', ' + movies[i]['votes'] + ', ' + movies[i]['running_times'];
+                myElement.innerHTML = content;
+                document.querySelector("#list").appendChild(myElement);
+        }
+
+//Exercise 5 - level 2 create button.
+        const button = document.querySelector('button')
+        console.log(button)
+        button.addEventListener('click', function() {
+                function getNewrMovies(newMovies){
+                return ['year'] > 2014 + movies
+                }
+        })
+    });
+
+
+
+
+
+
+
+
+
